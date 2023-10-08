e13b6a1862937270c7a69f0c15917207790b5d5a
Kudrot467 <kkudrotekhoda@gmail.com>
2023-10-08 19:37:21 +0600

    admission section added

--- src/Components/Admission/Admission.jsx
+++ src/Components/Admission/Admission.jsx
@@ -1,9 +1,51 @@
 const Admission = () => {
-    return (
-        <div>
-            <h3 className="text-4xl">This is admission</h3>
-        </div>
-    );
+  return (
+    <div className="overflow-x-auto max-w-6xl mx-auto my-10">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <h2 className="text-5xl font-semibold">Admission Schedule</h2>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+          <tr className="bg-base-200">
+            <td>Online Admission Form open</td>
+            <td>: October 04, 2023 (Wednesday)</td>
+          </tr>
+          {/* row 2 */}
+          <tr>
+            <td>Deadline for Online Admission Form Submission </td>
+            <td>: November 05, 2023 (Sunday) [Till 03:00 PM]</td>
+          </tr>
+          {/* row 3 */}
+          <tr>
+            <td>Deadline for Online Admission Form Payment </td>
+            <td>: November 07, 2023 (Tuesday) [Till 03:00 PM] </td>
+          </tr>
+          {/* row 4 */}
+          <tr>
+            <td>Admission & Registration </td>
+            <td>
+              : November 21 - 27, 2023 (Except Friday, Saturday & Govt.
+              Holidays){" "}
+            </td>
+          </tr>
+          {/* row 5 */}
+          <tr>
+            <td>Orientation</td>
+            <td>: specific schedule will be published later.</td>
+          </tr>
+          {/* row 6 */}
+          <tr>
+            <td>Class Starts</td>
+            <td>: January 21, 2024 (To be Confirmed)</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Admission;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,17 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "./Banner/Banner";
+import Course from "./Courses/Course/Course";
 
 const Home = () => {
-    const data=useLoaderData();
-    console.log(data);
+    const serviceData=useLoaderData()
+   
     return (
         <div>
             <Banner></Banner>
-            <h3 className="text-3xl">This is home</h3>
+            <h2 className="text-4xl font-semibold text-center">Our Running Services</h2>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-10">
+               { serviceData.map(course=><Course key={course.id} course={course}></Course>)}
+            </div>
         </div>
     );
 };
