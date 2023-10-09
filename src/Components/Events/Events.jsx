fdc603cf5a7b39719332f30efdbf111a099c6bf1
Kudrot467 <kkudrotekhoda@gmail.com>
2023-10-09 22:34:58 +0600

    events card some modify added

--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -16,10 +16,10 @@ const Events = ({ event }) => {
           <img className="w-full md:w-[550px] h-[250px]" src={pic} />
         </figure>
         <div className="card-body w-full">
-          <h2 className="card-title h-[90px]">{name}</h2>
-          <div>
+          <h2 className="card-title  mb-2 h-[90px]">{name}</h2>
+          <div className=" mt-0 md:mt-10" >
             {description.length > 150 ? (
-              <p className="">
+              <p className=" my-8 md:my-0">
                 {description.slice(0, 150)}{" "}
                 <Link to={`/events/${id}`}>
                   <div className="card-actions justify-end">
